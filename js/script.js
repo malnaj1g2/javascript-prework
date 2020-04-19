e8d59bda39bf591e88be6d46eefb9b298f2a6ac4
Małgorzata Najda-Góra <malnaj1g2@gmail.com>
2020-04-19 22:20:20 +0200

    modify  js/script.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+var playerMove;
+playerMove = 'kamień';
+printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+playerMove = 'papier';
+printMessage('zagrałeś' + playerMove + '! Jeśli mój ruch to nożyce, to wygrywam !');
