9f1478a4dea825be487d2f050b46ab4097ec901a
Małgorzata Najda-Góra <malnaj1g2@gmail.com>
2020-04-21 21:54:31 +0200

    add update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
-var playerMove;
-playerMove = 'kamień';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
-playerMove = 'papier';
-printMessage('zagrałeś' + playerMove + '! Jeśli mój ruch to nożyce, to wygrywam !');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('wylosowana liczba to: ' + randomNumber);
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else {
+  computerMove = 'nieznany ruch';
+}
+printMessage('Mój ruch: ' + computerMove);
+computerMove = Math.floor(Math.random() * 2 + 1);
+console.log('Wylosowana liczba to:' + randomNumber);
+if (randomNumber == '2') {
+  computerMove = ' papier';
+} else {
+  computerMove = ' nieznany ruch';
+}
+printMessage(' Mój ruch:' + computerMove);
+computerMove = Math.floor((Math.random() + 1) * 1);
+console.log(' Wylosowana liczba to:' + randomNumber);
+if (randomNumber == '3') {
+  computerMove = ' nożyce';
+} else {
+  computerMove = ' nieznany ruch';
+}
+printMessage(' Mój ruch:' + computerMove);
+computerMove = 'nieznany ruch';
