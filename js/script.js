1de85500a06d214c1b6eabb982a0c8874cbee225
Małgorzata Najda-Góra <malnaj1g2@gmail.com>
2020-04-21 22:31:27 +0200

    add new script.js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,33 @@ if (randomNumber == '3') {
 }
 printMessage(' Mój ruch:' + computerMove);
 computerMove = 'nieznany ruch';
+
+var playerInput, playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to:' + playerInput);
+if (playerInput == '2') {
+  playerMove = ' papier';
+} else if (playerInput > '3') {
+  playerMove = ' błąd';
+} else {
+  playerMove = ' nieznany ruch';
+}
+printMessage(' Twój ruch: ' + playerMove);
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to:' + playerInput);
+if (playerInput == '3') {
+  playerMove = ' nożyce';
+} else if (playerInput > '3') {
+  playerMove = 'błąd';
+} else {
+  playerMove = ' nieznany ruch ';
+}
+printMessage(' Twój ruch: ' + playerMove);
