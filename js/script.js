ab096ebf188e0ffceb42d0161a4cb6c1cb5307ec
Małgorzata Najda-Góra <malnaj1g2@gmail.com>
2020-04-23 19:05:04 +0200

    add player js/script.js

--- js/script.js
+++ js/script.js
@@ -1,56 +1,27 @@
-var computerMove, randomNumber;
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('wylosowana liczba to: ' + randomNumber);
-if (randomNumber == '1') {
-  computerMove = 'kamień';
-} else {
-  computerMove = 'nieznany ruch';
-}
-printMessage('Mój ruch: ' + computerMove);
-computerMove = Math.floor(Math.random() * 2 + 1);
-console.log('Wylosowana liczba to:' + randomNumber);
-if (randomNumber == '2') {
-  computerMove = ' papier';
-} else {
-  computerMove = ' nieznany ruch';
-}
-printMessage(' Mój ruch:' + computerMove);
-computerMove = Math.floor((Math.random() + 1) * 1);
-console.log(' Wylosowana liczba to:' + randomNumber);
-if (randomNumber == '3') {
-  computerMove = ' nożyce';
-} else {
-  computerMove = ' nieznany ruch';
-}
-printMessage(' Mój ruch:' + computerMove);
-computerMove = 'nieznany ruch';
+function displayResult(argPlayerMove, argComputerMove) {
 
-var playerInput, playerMove;
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to: ' + playerInput);
-if (playerInput == '1') {
-  playerMove = 'kamień';
-} else {
-  playerMove = 'nieznany ruch';
+  if(argPlayerMove === 1 && argComputerMove === 2) {
+    console.log('Przegrywasz');
+  }
+  else if(argPlayerMove === 2 && argComputerMove === 1) {
+    console.log('Wygrywasz')
+  }
+  else if(argPlayerMove === 3 && argComputerMove === 1) {
+    console.log('Przegrywasz')
+  }
+  else if(argPlayerMove === 3 && argComputerMove === 2) {
+    console.log('Wygrywasz')
+  }
+  else if(argPlayerMove === 1 && argComputerMove === 3) {
+    console.log('Wygrywasz')
+  }
+  else if(argPlayerMove === 2 && argComputerMove === 3) {
+    console.log('Przegrywasz')
+  }
+  else {
+    console.log('Remis')
+  }
+
 }
-printMessage('Twój ruch: ' + playerMove);
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to:' + playerInput);
-if (playerInput == '2') {
-  playerMove = ' papier';
-} else if (playerInput > '3') {
-  playerMove = ' błąd';
-} else {
-  playerMove = ' nieznany ruch';
-}
-printMessage(' Twój ruch: ' + playerMove);
-playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-console.log('Wpisana odpowiedź to:' + playerInput);
-if (playerInput == '3') {
-  playerMove = ' nożyce';
-} else if (playerInput > '3') {
-  playerMove = 'błąd';
-} else {
-  playerMove = ' nieznany ruch ';
-}
-printMessage(' Twój ruch: ' + playerMove);
+
+displayResult(1, 2)
