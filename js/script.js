b6261c27341943b45d47f160367dd28cd6e5815d
Małgorzata Najda-Góra <malnaj1g2@gmail.com>
2020-04-23 20:10:27 +0200

    add modified

--- js/script.js
+++ js/script.js
@@ -1,27 +1,49 @@
-function displayResult(argPlayerMove, argComputerMove) {
-
-  if(argPlayerMove === 1 && argComputerMove === 2) {
-    console.log('Przegrywasz');
-  }
-  else if(argPlayerMove === 2 && argComputerMove === 1) {
-    console.log('Wygrywasz')
-  }
-  else if(argPlayerMove === 3 && argComputerMove === 1) {
-    console.log('Przegrywasz')
-  }
-  else if(argPlayerMove === 3 && argComputerMove === 2) {
-    console.log('Wygrywasz')
-  }
-  else if(argPlayerMove === 1 && argComputerMove === 3) {
-    console.log('Wygrywasz')
-  }
-  else if(argPlayerMove === 2 && argComputerMove === 3) {
-    console.log('Przegrywasz')
-  }
-  else {
-    console.log('Remis')
-  }
-
-}
 
 displayResult(1, 2)
+
+
+var argButtonName, buttonTest;
+
+/**
+ * Describe this function...
+ */
+function buttonClicked(argButtonName) {
+  clearMessages();
+  PlayerMove = argButtonNAme;
+  console.log(argButtonName + ' został kliknięty');
+  function displayResult(argPlayerMove, argComputerMove) {
+
+    if(argPlayerMove === 1 && argComputerMove === 2) {
+      console.log('Przegrywasz');
+    }
+    else if(argPlayerMove === 2 && argComputerMove === 1) {
+      console.log('Wygrywasz')
+    }
+    else if(argPlayerMove === 3 && argComputerMove === 1) {
+      console.log('Przegrywasz')
+    }
+    else if(argPlayerMove === 3 && argComputerMove === 2) {
+      console.log('Wygrywasz')
+    }
+    else if(argPlayerMove === 1 && argComputerMove === 3) {
+      console.log('Wygrywasz')
+    }
+    else if(argPlayerMove === 2 && argComputerMove === 3) {
+      console.log('Przegrywasz')
+    }
+    else {
+      console.log('Remis')
+    }
+
+  }
+
+
+}
+buttonTest = document.getElementById('button-test');
+buttonTest.addEventListener('click', function(){ buttonClicked('Guzik TEST'); });
+argButtonName = document.getElementById(' button-rock');
+argButtonName.addEventListener('click', function(){ buttonClicked(' kamień'); });
+argButtonName = document.getElementById(' button-paer');
+argButtonName.addEventListener('click', function(){ buttonClicked('papier'); });
+argButtonName = document.getElementById('button-scissors');
+argButtonName.addEventListener('click', function(){ buttonClicked(' nożyce'); });
